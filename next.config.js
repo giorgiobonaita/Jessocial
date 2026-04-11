7d338219d6bc315aaec657e6b476d542cf6423ce
giorgiobonaita <giorgio.bonaita@gmail.com>
2026-04-11 16:32:59 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cunftokrdqvprepcnlum.supabase.co' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
